f60e23dd482533d6bb47e1ebc47d894f896d45a4
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2019-11-25 15:16:43 +0200

    lists length

--- src/main/scala/com/rockthejvm/lists/ListProblems.scala
+++ src/main/scala/com/rockthejvm/lists/ListProblems.scala
@@ -3,28 +3,42 @@ package com.rockthejvm.lists
 import scala.annotation.tailrec
 
 sealed abstract class RList[+T] {
+  /**
+    * Standard functions
+    */
   def head: T
   def tail: RList[T]
   def isEmpty: Boolean
-
   def ::[S >: T](elem: S): RList[S] = new ::(elem, this)
 
+  /**
+    * Easy problems
+    */
+  // get element at a given index
   def apply(index: Int): T
+
+  // the size of the list
+  def length: Int
 }
 
 case object RNil extends RList[Nothing] {
   override def head: Nothing = throw new NoSuchElementException
   override def tail: RList[Nothing] = throw new NoSuchElementException
   override def isEmpty: Boolean = true
-
   override def toString: String = "[]"
 
+  /**
+    * Easy problems
+    */
+  // get element at a given index
   override def apply(index: Int): Nothing = throw new NoSuchElementException
+
+  // the size of the list
+  override def length: Int = 0
 }
 
 case class ::[+T](override val head: T, override val tail: RList[T]) extends RList[T] {
   override def isEmpty: Boolean = false
-
   override def toString: String = {
     @tailrec
     def toStringTailrec(remaining: RList[T], result: String): String = {
@@ -36,6 +50,10 @@ case class ::[+T](override val head: T, override val tail: RList[T]) extends RLi
     "[" + toStringTailrec(this, "") + "]"
   }
 
+  /**
+    * Easy problems
+    */
+  // get element at a given index
   override def apply(index: Int): T = {
     /*
       [1,2,3,4,5].apply(2) = applyTailrec([1,2,3,4,5], 0)
@@ -55,14 +73,39 @@ case class ::[+T](override val head: T, override val tail: RList[T]) extends RLi
     if (index < 0) throw new NoSuchElementException
     else applyTailrec(this, 0)
   }
+
+  // the size of the list
+  override def length: Int = {
+    /*
+      [1,2,3,4,5].length = lengthTailrec([1,2,3,4,5], 0)
+      = lengthTailrec([2,3,4,5], 1)
+      = lengthTailrec([3,4,5], 2)
+      = lengthTailrec([4,5], 3)
+      = lengthTailrec([5], 4)
+      = lengthTailrec([], 5)
+      = 5
+
+      Complexity: O(N)
+     */
+    @tailrec
+    def lengthTailrec(remainingList: RList[T], accumulator: Int): Int = {
+      if (remainingList.isEmpty) accumulator
+      else lengthTailrec(remainingList.tail, accumulator + 1)
+    }
+
+    lengthTailrec(this, 0)
+  }
 }
 
 object ListProblems extends App {
 
   val aSmallList = 1 :: 2 :: 3 :: RNil // RNil.::(3).::(2).::(1)
 
+  // test get-kth
   println(aSmallList.apply(0))
   println(aSmallList.apply(2))
-  println(aSmallList.apply(90))
+
+  // test length
+  println(aSmallList.length)
 
 }
