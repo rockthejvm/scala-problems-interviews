f2bb55cffa05f0cc230f5ce0e60e12087d79109b
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2019-11-25 14:36:20 +0200

    lists get kth:

--- src/main/scala/com/rockthejvm/lists/ListProblems.scala
+++ src/main/scala/com/rockthejvm/lists/ListProblems.scala
@@ -8,6 +8,8 @@ sealed abstract class RList[+T] {
   def isEmpty: Boolean
 
   def ::[S >: T](elem: S): RList[S] = new ::(elem, this)
+
+  def apply(index: Int): T
 }
 
 case object RNil extends RList[Nothing] {
@@ -16,6 +18,8 @@ case object RNil extends RList[Nothing] {
   override def isEmpty: Boolean = true
 
   override def toString: String = "[]"
+
+  override def apply(index: Int): Nothing = throw new NoSuchElementException
 }
 
 case class ::[+T](override val head: T, override val tail: RList[T]) extends RList[T] {
@@ -31,11 +35,34 @@ case class ::[+T](override val head: T, override val tail: RList[T]) extends RLi
 
     "[" + toStringTailrec(this, "") + "]"
   }
+
+  override def apply(index: Int): T = {
+    /*
+      [1,2,3,4,5].apply(2) = applyTailrec([1,2,3,4,5], 0)
+      = applyTailrec([2,3,4,5], 1)
+      = applyTailrec([3,4,5], 2)
+      = 3
+
+      Complexity of this algorithm?
+      O(min(N, index))
+     */
+    @tailrec
+    def applyTailrec(remaining: RList[T], currentIndex: Int): T = {
+      if (currentIndex == index) remaining.head
+      else applyTailrec(remaining.tail, currentIndex + 1)
+    }
+
+    if (index < 0) throw new NoSuchElementException
+    else applyTailrec(this, 0)
+  }
 }
 
 object ListProblems extends App {
 
   val aSmallList = 1 :: 2 :: 3 :: RNil // RNil.::(3).::(2).::(1)
-  println(aSmallList)
+
+  println(aSmallList.apply(0))
+  println(aSmallList.apply(2))
+  println(aSmallList.apply(90))
 
 }
