672d69603256e720817fbaf7f09074d55f091b59
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2019-11-26 17:06:44 +0200

    lists rotate

--- src/main/scala/com/rockthejvm/lists/ListProblems.scala
+++ src/main/scala/com/rockthejvm/lists/ListProblems.scala
@@ -42,6 +42,9 @@ sealed abstract class RList[+T] {
 
   // duplicate each element a number of times in a row
   def duplicateEach(k: Int): RList[T]
+
+  // rotation by a number of positions to the left
+  def rotate(k: Int): RList[T]
 }
 
 case object RNil extends RList[Nothing] {
@@ -81,6 +84,9 @@ case object RNil extends RList[Nothing] {
 
   // duplicate each element a number of times in a row
   override def duplicateEach(k: Int): RList[Nothing] = RNil
+
+  // rotate by a number of positions to the left
+  override def rotate(k: Int): RList[Nothing] = RNil
 }
 
 case class ::[+T](override val head: T, override val tail: RList[T]) extends RList[T] {
@@ -321,6 +327,43 @@ case class ::[+T](override val head: T, override val tail: RList[T]) extends RLi
 
     duplicateTailrec(this.tail, this.head, 0, RNil)
   }
+
+  // rotate by a number of positions to the left
+  override def rotate(k: Int): RList[T] = {
+    /*
+      [1,2,3].rotate(3) == [1,2,3]
+      [1,2,3].rotate(6) == [1,2,3]
+      [1,2,3].rotate(4) == [1,2,3].rotate(1)
+
+      [1,2,3].rotate(1) = rotateTailrec([1,2,3], 1, [])
+      = rotateTailrec([2,3], 0, [1])
+      = [2,3,1]
+
+      [1,2,3].rotate(3) = rotateTailrec([1,2,3], 3, [])
+      = rotateTailrec([2,3], 2, [1])
+      = rotateTailrec([3], 1, [2,1])
+      = rotateTailrec([], 0, [3,2,1])
+      = [1,2,3]
+
+      [1,2,3].rotate(4) = rotateTailrec([1,2,3], 4, [])
+      = rotateTailrec([2,3], 3, [1])
+      = rotateTailrec([3], 2, [2,1])
+      = rotateTailrec([], 1, [3,2,1])
+      = rotateTailrec([1,2,3], 1, [])
+      = [2,3,1]
+
+      Complexity: O(max(N, K))
+     */
+    @tailrec
+    def rotateTailrec(remaining: RList[T], rotationsLeft: Int, buffer: RList[T]): RList[T] = {
+      if (remaining.isEmpty && rotationsLeft == 0) this
+      else if (remaining.isEmpty) rotateTailrec(this, rotationsLeft, RNil)
+      else if (rotationsLeft == 0) remaining ++ buffer.reverse
+      else rotateTailrec(remaining.tail, rotationsLeft - 1, remaining.head :: buffer)
+    }
+
+    rotateTailrec(this, k, RNil)
+  }
 }
 
 object RList {
@@ -337,6 +380,7 @@ object RList {
 object ListProblems extends App {
   val aSmallList = 1 :: 2 :: 3 :: RNil // RNil.::(3).::(2).::(1)
   val aLargeList = RList.from(1 to 10000)
+  val oneToTen = RList.from(1 to 10)
 
   def testEasyFunctions() = {
     // test get-kth
@@ -376,6 +420,10 @@ object ListProblems extends App {
     println((1 :: 1 :: 1 :: 2 :: 3 :: 3 :: 4 :: 5 :: 5 :: 5 :: RNil).rle)
     // duplicateEach
     println(aSmallList.duplicateEach(4))
+    // rotate
+    for {
+      i <- 1 to 20
+    } println(oneToTen.rotate(i))
   }
 
   testMediumDifficultyFunctions()
