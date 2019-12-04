1fd05ecd93fade6c9709ad8134c468a504bb86ae
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2019-12-04 18:53:17 +0200

    graph paths

--- src/main/scala/com/rockthejvm/graphs/GraphProblems.scala
+++ src/main/scala/com/rockthejvm/graphs/GraphProblems.scala
@@ -1,5 +1,7 @@
 package com.rockthejvm.graphs
 
+import scala.annotation.tailrec
+
 object GraphProblems extends App {
 
   type Graph[T] = Map[T, Set[T]]
@@ -15,7 +17,7 @@ object GraphProblems extends App {
   /**
     * Easy problems
     */
-  
+
   // number of nodes this node `node` is associated (adjacent) to
   def outDegree[T](graph: Graph[T], node: T): Int =
     if (graph.contains(node)) graph(node).size
@@ -28,5 +30,41 @@ object GraphProblems extends App {
   println(outDegree(socialNetwork, "Alice")) // 3
   println(inDegree(socialNetwork, "David")) // 2
 
+  /**
+    * Medium difficulty problems
+    */
+
+  def isPath[T](graph: Graph[T], start: T, end: T): Boolean = {
+    /*
+      Alice -> Mary
+
+      ipt([Alice], []) =
+      ipt([Bob, Charlie, David], [Alice]) =
+      ipt([Charlie, David], [Bob, Alice]) =
+      ipt([David, David], [Charlie, Bob, Alice]) =
+      ipt([David, Bob, Mary], [David, Charlie, Bob, Alice]) =
+      ipt([Bob, Mary], [David, Charlie, Bob, Alice]) =
+      ipt([Mary], [David, Charlie, Bob, Alice]) =
+      = true
+
+      N nodes, E edges
+      Complexity: O(E)
+     */
+    @tailrec
+    def isPathTailrec(remaining: List[T], consideredNodes: Set[T]): Boolean = {
+      if (remaining.isEmpty) false
+      else {
+        val node = remaining.head
+        if (node == end) true
+        else if (consideredNodes.contains(node)) isPathTailrec(remaining.tail, consideredNodes)
+        else isPathTailrec(remaining.tail ++ graph(node), consideredNodes + node)
+      }
+    }
+
+    isPathTailrec(List(start), Set())
+  }
+
+  println(isPath(socialNetwork, "Alice", "Mary")) // true
+  println(isPath(socialNetwork, "Bob", "Mary")) // false
 
 }
