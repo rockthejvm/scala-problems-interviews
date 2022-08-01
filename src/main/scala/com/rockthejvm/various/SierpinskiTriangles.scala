e691f9ac4b5c362a366c568940a4287cc4d5e7b4
Janik Dotzel <janik.dotzel@lightbend.com>
2022-08-01 15:01:02 +0200

    refactor: initialize all various problems

--- src/main/scala/com/rockthejvm/various/SierpinskiTriangles.scala
+++ src/main/scala/com/rockthejvm/various/SierpinskiTriangles.scala
@@ -25,30 +25,7 @@ object SierpinskiTriangles extends App {
           * *
 
    */
-  def sierpinski(n: Int): String = {
-    def sierpinskiStack(level: Int): List[String] = {
-      if (level == 0) List("*")
-      else {
-        val triangle = sierpinskiStack(level - 1)
-        val spaces = " " * (1 << (level - 1)) // 2 ^ (n-1) spaces
-        val topTriangle = triangle.map(spaces + _ + spaces)
-        val bottomTriangles = triangle.map(row => row + " " + row)
-        topTriangle ++ bottomTriangles
-      }
-    }
-
-    def sierpinskiTail(currentLevel: Int, currentTriangle: List[String]): List[String] = {
-      if (currentLevel >= n) currentTriangle
-      else {
-        val spaces = " " * (1 << currentLevel) // 2 ^ (n-1) spaces
-        val topTriangle = currentTriangle.map(spaces + _ + spaces)
-        val bottomTriangles = currentTriangle.map(row => row + " " + row)
-        sierpinskiTail(currentLevel + 1, topTriangle ++ bottomTriangles)
-      }
-    }
-
-    sierpinskiTail(0, List("*")).mkString("\n")
-  }
+  def sierpinski(n: Int): String = ???
 
   println(sierpinski(8))
 
